fd1cf0f949ec29eb33fc377655da9e3e58015a32
LuisoDev1 <luisbet92@gmail.com>
2025-12-27 05:14:12 +0000

    corigiendo 2

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -6,8 +6,7 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-            
+		<div className="text-center">            
 
 			<h1 className="text-center mt-5">Hello Rigo!</h1>
 			<p>
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,71 +1,94 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 
-//create your first component
+// create your first component
 const Home = () => {
-
 	const [task, setTask] = useState("");
-	const [taskList, setTaskList] = useState( {} );
-	
-	{/*FUNCION PARA AGREGAR TAREAS*/}
-	function addTask async () {
-    const addTask = await fetch(URL, {
-      method: "POST",
-      headers: {
-        "content-type": "aplication/json"
-      },
-      body: JSON.stringify( {label:task, is_done:false} )
-    })
-		setTaskList([...taskList, {} ])
+	const [taskList, setTaskList] = useState([]);
 
-	}
-	{/*FUNCION PARA BORRAR TAREAS en tasklist, ES LLAMADA DESDE EL onClick del */}
-	const deleteTask = async (itemDelete) => {
-    const response = await fetch(URL)
-    method: "DELETE"    
-		let newlistTask = taskList.filter(item => item != itemDelete)
-		setTaskList(newlistTask)
-		}	
+
+	const URL = "URL_AQUI"; // No olvidar
+
+
+	// FUNCIÓN PARA AGREGAR TAREAS
+	const addTask = async () => {
+		if (task.trim() === "") return;
+		const newTask = { label: task, is_done: false}		
+		await fetch(URL, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(newTask)
+		});
+		setTaskList([...taskList, newTask]);
+		setTask("");
+	};
+	
+
+	// FUNCIÓN PARA BORRAR TAREAS
+	const deleteTask = async (taskToDelete) => {
+		await fetch(URL, {
+			method: "DELETE"
+		});
+		const newTaskList = taskList.filter((item) => item !== taskToDelete);
+		setTaskList(newTaskList); // Agrego al etado taskList la nueva lista filtrada
+	};
 
 	return (
-		<div className="text-center d-flex flex-column">           
+		<div className="text-center d-flex flex-column">
 
-			<h1 className="text-center mt-5 text-success" style={{fontSize:"55px", userSelect:"none"}} >Todo List</h1>
-			<input 
-				type="text" 
-				className="form-control w-25 mx-auto" 
-				onChange={(e) =>  setTask(e.target.value)}  
+			<h1
+				className="text-center mt-5 text-success"
+				style={{ fontSize: "55px", userSelect: "none" }}
+			    >Todo List
+			</h1>
+
+			<input
+				type="text"
+				className="form-control w-25 mx-auto"
+				value={task}
+				onChange={(e) => setTask(e.target.value)}
 				onKeyUp={(e) => {
-					if(e.target.key=="enter") {
-						setTaskList([...taskList, task])
-					}
-				}}				
-				/>
-			
-			<div className="d-flex justify-content-center my-3" >
-				<button className="btn btn-success p-0" style={{width:"80px", height:"40px"}} onClick={(addTask) }>Add Task</button>
+					if (e.key === "Enter") addTask();
+				}}
+			/>
+
+			<div className="d-flex justify-content-center my-3">
+				<button
+					className="btn btn-success p-0"
+					style={{ width: "80px", height: "40px" }}
+					onClick={addTask}
+					>Add Task
+				</button>
 			</div>
-			
-			<p>{task}</p>
-			
-			{taskList.map((taskItem, index) => {
-				return (
-				<div className="d-flex alig-items-center justify-content-center" >
-					<p className="mt-3 text-success" key={index} 
-						style={{border:"2px solid #000", width:"20%", borderRadius:"8px", fontWeight:"bold", userSelect:"none"}}> 
-						{taskItem} 
-						<div >
-							<button type="button" 
-							className="btn text-center p-0 btn-close " 
-							style={{ width:"60px", color:"#ddd5d5ff"}}  
-							onClick={() => deleteTask(taskItem)} ></button>
-						</div>
-					</p>			
+
+			{/* Renderizando la lista de tareas a traves de map */}
+			{taskList.map((taskItem, index) => (
+				<div
+					key={index}
+					className="d-flex align-items-center justify-content-center"
+				    >
+					<p
+						className="mt-3 text-success"
+						style={{
+							border: "2px solid #000",
+							width: "20%",
+							borderRadius: "8px",
+							fontWeight: "bold",
+							userSelect: "none"
+						}}
+						>{taskItem.label}
+					</p>
+
+					<button
+						type="button"
+						className="btn text-center p-0 btn-close"
+						style={{ width: "60px" }}
+						onClick={() => deleteTask(taskItem)}
+					/>
 				</div>
-				)
-				
-			})}
-			
+			))}
+
 		</div>
 	);
 };
